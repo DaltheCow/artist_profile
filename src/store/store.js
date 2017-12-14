28610b3411ec4c66fa7f25e95e8908a96df47ad8
Ben Blue <dalcowboiz@gmail.com>
2017-12-14 12:30:39 -0500

    remove comments from store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
-// import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import RootReducer from '../reducers/rootReducer'
-
-// const middleware = (window.location.href.indexOf('http://localhost') === 0 ? [thunk, logger] : [thunk]);
+import RootReducer from '../reducers/rootReducer';
 
 const configureStore = (preloadedState = {}) => {
   return createStore(RootReducer, preloadedState, applyMiddleware(logger));
